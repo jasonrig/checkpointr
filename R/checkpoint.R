64eb2afbbb46add1e98d700fdf880083bdf993b9
Jason Rigby <jason.rigby@monash.edu>
2019-11-14 15:05:51 +1100

    initial commit

---
+++ R/checkpoint.R
@@ -0,0 +1,80 @@
+#' Evaluates an expression or loads a cached value. Both the change to the environment
+#' and the expression's return value is stored.
+#' The expression is re-evaluated if the expression or value of `deps` changes.
+#' @param expr any valid R expression
+#' @param ckpt.id an identifier used for the checkpointed data
+#' @param deps any R object that is monitored for changed and is used to trigger re-evaluation
+#' @param force force re-evaluation of the expression regardless of the state of `deps` or `expr`
+#' @param envir the environment in which the expression is evaluated
+#' @param enclos the environment in which the expression is substituted
+#' @return the result of the expression
+#' @export
+checkpoint <-
+  function(expr,
+           ckpt.id,
+           deps = NA,
+           force = FALSE,
+           envir = parent.frame(),
+           enclos = environment()) {
+    # Prepare the checkpoint file name
+    ckpt_file <- paste0(ckpt.id, ".dat")
+
+    # Set up an environment in which to store the result of the evaluated expression
+    tmp_env <- new.env(parent = envir)
+
+    # Substitute the expression
+    expr <- substitute(expr, enclos)
+
+    # Calculate the hash used to determine whether a re-evaluation is necessary
+    deps_hash <-
+      digest::digest(list(deps = deps, expr = deparse(expr)), "sha256")
+
+    if (!force && file.exists(ckpt_file)) {
+      # Load the checkpoint if the checkpoint file exists
+      message(paste0("Loading checkpoint ", ckpt.id, "."))
+      load(ckpt_file, envir = tmp_env)
+      ckpt_hash <- tmp_env$.hash
+
+      # Compare the hash to determine whether re-evaluation is necessary
+      if (deps_hash != ckpt_hash) {
+        message("Dependent variables have changed!")
+        return(checkpoint(
+          expr,
+          ckpt.id,
+          deps = deps,
+          force = TRUE,
+          envir = envir,
+          enclos = enclos
+        ))
+      }
+      retval <- tmp_env$.retval
+    } else {
+      # Load the data in (re-)evaluation is necessary
+      message("Evaluating expression.")
+      retval <- eval(expr, envir = tmp_env)
+      assign(".retval", retval, tmp_env)
+      assign(".hash", deps_hash, tmp_env)
+      save(
+        list = ls(tmp_env, all.names = TRUE),
+        envir = tmp_env,
+        file = ckpt_file,
+        ascii = TRUE
+      )
+    }
+
+    # Populate the parent environment
+    copy.env(tmp_env, envir)
+
+    return(retval)
+  }
+
+#' Helper function to copy the content of one environment to another
+#' @param source_env the environment from which data will be copied
+#' @param destination_env the environment to which data will be copied
+#' @param all.names whether or not to include hidden variables
+copy.env <-
+  function(source_env, destination_env, all.names = FALSE) {
+    sapply(ls(source_env, all.names = all.names), function(n) {
+      assign(n, get(n, source_env), destination_env)
+    })
+  }
---
+++ tests/testthat/test-ckpt.R
@@ -0,0 +1,91 @@
+context("checkpointing data")
+
+test_that("data that is checkpointed can be loaded again", {
+  x <- seq(0, 100)
+  y <- checkpoint({
+    m <- mean(x)
+    s <- sd(x)
+    42
+  }, "test_checkpoint1")
+  expect_equal(y, 42)
+  expect_equal(m, mean(seq(0, 100)))
+  expect_equal(s, sd(seq(0, 100)))
+
+  rm(x, y, m, s)
+
+  y <- checkpoint({
+    m <- mean(x)
+    s <- sd(x)
+    42
+  }, "test_checkpoint1")
+  expect_equal(y, 42)
+  expect_equal(m, mean(seq(0, 100)))
+  expect_equal(s, sd(seq(0, 100)))
+
+  file.remove("test_checkpoint1.dat")
+})
+
+test_that("the expression is re-evaluated when the dependent variables changes", {
+  x <- rnorm(100)
+  m <- checkpoint({
+    mean(x)
+  }, deps = 1, "test_checkpoint2")
+  expect_equal(m, mean(x))
+
+  x <- rnorm(100)
+  m <- checkpoint({
+    mean(x)
+  }, deps = 1, "test_checkpoint2")
+  expect_false(m == mean(x))
+
+  m <- checkpoint({
+    mean(x)
+  }, deps = 2, "test_checkpoint2")
+  expect_equal(m, mean(x))
+})
+
+test_that("the expression is re-evaluated when the expression changes", {
+  x <- rnorm(100)
+  m <- checkpoint({
+    mean(x)
+  }, "test_checkpoint3")
+  expect_equal(m, mean(x))
+
+  x <- rnorm(100)
+  y <- x
+  m <- checkpoint({
+    mean(x)
+  }, "test_checkpoint3")
+  expect_false(m == mean(x))
+
+  m <- checkpoint({
+    mean(y)
+  }, "test_checkpoint3")
+  expect_equal(m, mean(x))
+})
+
+test_that("the expression can be forcefully re-evaluated", {
+  m1 <- checkpoint({
+    rnorm(100)
+  }, "test_checkpoint4")
+  m2 <- checkpoint({
+    rnorm(100)
+  }, "test_checkpoint4")
+  m3 <- checkpoint({
+    rnorm(100)
+  }, "test_checkpoint4", force = TRUE)
+  m4 <- checkpoint({
+    rnorm(100)
+  }, "test_checkpoint4")
+
+  expect_true(all(sapply(m1, round, 4) == sapply(m2, round, 4)))
+  expect_true(all(sapply(m3, round, 4) == sapply(m4, round, 4)))
+  expect_false(all(sapply(m2, round, 4) == sapply(m3, round, 4)))
+})
+
+teardown({
+  unlink("test_checkpoint1.dat")
+  unlink("test_checkpoint2.dat")
+  unlink("test_checkpoint3.dat")
+  unlink("test_checkpoint4.dat")
+})
